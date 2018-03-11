e5f2520b493a35890f30585528a5e762503445a2
Ashymad <czilukim@o2.pl>
2018-03-11 16:29:05 +0100

    Even more cleanup + SIGINT handler

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 from mma8451 import accel
 import datetime
 import time
+import signal
+import sys
+
+def sigint_handler(signal, frame):
+    print('Exiting...')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 MMA8451 = accel.Accel()
 MMA8451.init()
+
+def printAcceleration(xaccel, yaccel, zaccel):
+    print("   x (m/s2)= %+.3f" % (xaccel))
+    print("   y (m/s2)= %+.3f" % (yaccel))
+    print("   z (m/s2)= %+.3f" % (zaccel))
 
 if MMA8451.whoAmI() != accel.deviceName:
     print("Error! Device not recognized! (" + str(accel.deviceName) + ")")
@@ -13,6 +26,6 @@ if MMA8451.whoAmI() != accel.deviceName:
 while True:  # forever loop
     print ("\nCurrent Date-Time: " + str(datetime.datetime.now()))
     axes = MMA8451.getAxisValue()
-    MMA8451.debugShowAxisAcceleration(axes['x'], axes['y'], axes['z'])
+    printAcceleration(axes['x'], axes['y'], axes['z'])
 
     time.sleep(0.5)
