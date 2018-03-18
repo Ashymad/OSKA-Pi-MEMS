ca3de84507e496e831d1a1e9c634234984e9d29b
Ashymad <czilukim@o2.pl>
2018-03-18 18:15:35 +0100

    Switch to rewrite

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ signal.signal(signal.SIGINT, sigint_handler)
 
 MMA8451 = accel.Accel()
 
-def printAcceleration(xaccel, yaccel, zaccel):
-    print("   x (m/s2)= %+.3f" % (xaccel))
-    print("   y (m/s2)= %+.3f" % (yaccel))
-    print("   z (m/s2)= %+.3f" % (zaccel))
-
-if MMA8451.whoAmI() != accel.deviceName:
-    print("Error! Device not recognized! (" + str(accel.deviceName) + ")")
-    sys.exit()
-
 MMA8451.init_callback()
 time.sleep(30)
+MMA8451.cleanup()
