13f593e91990c9bf3180a5cf1929b74195c48ee3
Ashymad <czilukim@o2.pl>
2018-03-20 00:24:31 +0100

    Filter actual data!

--- main.py
+++ main.py
@@ -48,22 +48,14 @@ while not data.empty():
 
 np.savetxt("out.txt",sigdta)
 
-f1 = 50
-f2 = 120
-f3 = 180
+fs = 800
 
-fs = 1000
+frq = acoustics.signal.OctaveBand(fstart=12.5, fstop=200, fraction=3)
 
-t = np.arange(0,2,0.001)
-y = np.sin(2*np.pi*t*f1) + np.sin(2*np.pi*t*f2) + np.sin(2*np.pi*t*f3)
-
-low_f = [11.2, 14.1, 17.8, 22.4, 28.2, 35.5, 44.7, 56.2, 70.8, 89.1, 112, 141, 178]
-cen_f = [12.5, 16,   20,   25,   31.5, 40,   50,   63,   80,   100,  125, 160, 200]
-upp_f = [14.1, 17.8, 22.4, 28.2, 35.5, 44.7, 56.2, 70.8, 89.1, 112,  141, 178, 224]
-
-frq = acoustics.signal.Frequencies(low_f,cen_f,upp_f)
 frq.sample_frequency = fs
+
 filter = acoustics.signal.Filterbank(frq)
-y_f = filter.power(y)
+
+y_f = filter.power(sigdta[:,0])
 
 print(y_f)
