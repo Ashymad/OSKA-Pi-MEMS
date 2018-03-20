84844e5fa75c1e4a70f62836d5fda7fe3151baa5
Ashymad <czilukim@o2.pl>
2018-03-20 22:31:50 +0100

    Save to WAV

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import signal
 from scipy.constants import g
+from scipy.io import wavfile
 import numpy as np
 import sys
 
@@ -18,13 +19,14 @@ signal.signal(signal.SIGINT, sigint_handler)
 MMA8451 = accel.Accel()
 
 MMA8451.init_callback()
-time.sleep(5)
+time.sleep(60)
 MMA8451.cleanup()
 
 data = MMA8451.getQueue()
 
 divider = 4096/g
 prec = 14
+fs = 800
 
 def reg2float(msb, lsb):
     num = ((msb << 8) | lsb) >> 2
@@ -41,23 +43,43 @@ def prepare_data(bitvec):
         data[i//6,2] = reg2float(bitvec[i+4],bitvec[i+5])
     return data
 
-sigdta = prepare_data(data.get())
+def reg2uint(msb, lsb):
+    num = ((msb << 8) | lsb) >> 2
+    maxn = 2**(prec-1)-1
+    signed_maxn = 2**prec
+    num -= signed_maxn if num > maxn else 0
+    return np.int16(num)
+
+def prepare_wavd(bitvec):
+    data = np.zeros([len(bitvec)//6,3], dtype=np.int16)
+    for i in range(0, len(bitvec), 6):
+        data[i//6,0] = reg2uint(bitvec[i],bitvec[i+1])
+        data[i//6,1] = reg2uint(bitvec[i+2],bitvec[i+3])
+        data[i//6,2] = reg2uint(bitvec[i+4],bitvec[i+5])
+    return data
+
+sigdta = prepare_wavd(data.get())
+
 while not data.empty():
-    sigdta = np.append(sigdta,prepare_data(data.get()), axis=0)
+    sigdta = np.append(sigdta,prepare_wavd(data.get()), axis=0)
 
-np.savetxt("out.txt",sigdta)
-
-fs = 800
-
-frq = acoustics.signal.OctaveBand(fstart=0.5, fstop=100, fraction=3)
-
-filt = acoustics.signal.Filterbank(frq, sample_frequency=fs)
-
-y_f = filt.lfilter(sigdta[:,0])
-
-filt_data = list(y_f)
-
-result = np.zeros(len(filt_data))
-for it in range(len(filt_data)):
-    result[it] = np.sqrt(np.mean(np.square(filt_data[it])))
-    print(result[it])
+wavfile.write("x_data.wav",fs,sigdta[:,0])
+wavfile.write("y_data.wav",fs,sigdta[:,1])
+wavfile.write("z_data.wav",fs,sigdta[:,2])
+#
+#np.savetxt("out.txt",sigdta)
+#
+#
+#frq = acoustics.signal.OctaveBand(fstart=0.5, fstop=100, fraction=3)
+#
+#filt = acoustics.signal.Filterbank(frq, sample_frequency=fs)
+#
+#y_f = filt.filtfilt(sigdta[:,0])
+#
+#filt_data = list(y_f)
+#
+#result = np.zeros(len(filt_data))
+#for it in range(len(filt_data)):
+#    #result[it] = np.sqrt(np.mean(np.square(filt_data[it])))
+#    result[it] = np.max(filt_data[it])
+#    print(result[it])
