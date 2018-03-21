74c9d08869892ad40acaa53ad7996bb418db0163
Ashymad <czilukim@o2.pl>
2018-03-21 21:22:36 +0100

    Cleanup

--- code/main.py
+++ code/main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 from mma8451 import mma8451
-import acoustics.signal
 import time
 import signal
-from scipy.constants import g
-from scipy.io import wavfile
-import numpy as np
-import h5py as h5
 import sys
 
 def sigint_handler(signal, frame):
@@ -19,68 +14,6 @@ signal.signal(signal.SIGINT, sigint_handler)
 MMA8451 = mma8451.Accel()
 
 MMA8451.init_callback()
-t = 600
+t = 300
 time.sleep(t)
 MMA8451.cleanup()
-
-data = MMA8451.getQueue()
-
-divider = 4096/g
-prec = 14
-fs = 800
-
-def reg2float(msb, lsb):
-    num = ((int(msb) << 8) | int(lsb)) >> 2
-    maxn = 2**(prec-1)-1
-    signed_maxn = 2**prec
-    num -= signed_maxn if num > maxn else 0
-    return float(num)/divider
-
-def prepare_data(bitvec):
-    data = np.zeros([len(bitvec)//6,3])
-    for i in range(0, len(bitvec), 6):
-        data[i//6,0] = reg2float(bitvec[i],bitvec[i+1])
-        data[i//6,1] = reg2float(bitvec[i+2],bitvec[i+3])
-        data[i//6,2] = reg2float(bitvec[i+4],bitvec[i+5])
-    return data
-
-def reg2uint(msb, lsb):
-    num = ((int(msb) << 8) | int(lsb)) >> 2
-    maxn = 2**(prec-1)-1
-    signed_maxn = 2**prec
-    num -= signed_maxn if num > maxn else 0
-    return np.int16(num)
-
-def prepare_wavd(bitvec):
-    data = np.zeros([len(bitvec)//6,3], dtype=np.int16)
-    for i in range(0, len(bitvec), 6):
-        data[i//6,0] = reg2uint(bitvec[i],bitvec[i+1])
-        data[i//6,1] = reg2uint(bitvec[i+2],bitvec[i+3])
-        data[i//6,2] = reg2uint(bitvec[i+4],bitvec[i+5])
-    return data
-
-sigdta = prepare_wavd(data.get())
-
-while not data.empty():
-    sigdta = np.append(sigdta,prepare_wavd(data.get()), axis=0)
-
-wavfile.write("x_data.wav",fs,sigdta[:,0])
-wavfile.write("y_data.wav",fs,sigdta[:,1])
-wavfile.write("z_data.wav",fs,sigdta[:,2])
-#
-#np.savetxt("out.txt",sigdta)
-#
-#
-#frq = acoustics.signal.OctaveBand(fstart=0.5, fstop=100, fraction=3)
-#
-#filt = acoustics.signal.Filterbank(frq, sample_frequency=fs)
-#
-#y_f = filt.filtfilt(sigdta[:,0])
-#
-#filt_data = list(y_f)
-#
-#result = np.zeros(len(filt_data))
-#for it in range(len(filt_data)):
-#    #result[it] = np.sqrt(np.mean(np.square(filt_data[it])))
-#    result[it] = np.max(filt_data[it])
-#    print(result[it])
