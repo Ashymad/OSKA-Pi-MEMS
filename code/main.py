01e7b94ca71e0fba88cf0788d03f09662cf6b974
Ashymad <czilukim@o2.pl>
2018-03-22 17:50:50 +0100

    Fix the crashing ctrl+c

--- code/main.py
+++ code/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from mma8451 import mma8451
-from threading import Lock
 import time
 import signal
 import sys
@@ -10,12 +9,11 @@ def sigint_handler(signal, frame):
     MMA8451.cleanup()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, sigint_handler)
 
 MMA8451 = mma8451.Accel()
 
+signal.signal(signal.SIGINT, signal.SIG_IGN)
 MMA8451.init_callback()
+signal.signal(signal.SIGINT, sigint_handler)
 print('Finished initialization')
-t = 60*60*8
-time.sleep(t)
-MMA8451.cleanup()
+signal.pause()
