74c1dc3a3b62bbfacb788291064ad740169ee911
Ashymad <czilukim@o2.pl>
2018-03-23 20:07:34 +0100

    Move to module

--- code/plotter.py
+++ code/plotter.py
@@ -1,44 +1,7 @@
-import numpy as np
 import h5py as h5
-import acoustics as ac
-
-
-def datamerge(h5g, *items):
-    items = list(items)
-    i = 0
-    dsets = list()
-    size = 0
-    while i < len(items):
-        it = h5g[items[i]]
-        if type(it) is h5.Dataset:
-            dsets.append(it)
-            size += len(it)
-        else:
-            for key in it.keys():
-                items.append(items[i] + "/" + key)
-        i += 1
-    dta = np.zeros([size, 3])
-    start = 0
-    for dset in dsets:
-        l = len(dset)
-        dta[start:start+l,:] = dset
-        start += l
-    return dta
-
-def dataread(f, *args):
-    if type(args[len(args)-1]) is not list:
-        args = (*args,[0, 9999])
-    g = f
-    for arg in range(0, len(args)-1):
-        g = g[args[arg]]
-    gr = list()
-    lim = args[len(args)-1]
-    for k in g.keys():
-        if int(k) > lim[1]:
-            break
-        if int(k) >= lim[0]:
-            gr.append(k)
-    return datamerge(g, *gr)
+from mma8451.files import dataread
 
 with h5.File('data.h5','r') as f:
-    print(dataread(f, "2018", '03', '23'))
+    data = dataread(f, "2018", '03', '23', [1,2])
+
+print(data)
