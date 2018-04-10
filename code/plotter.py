aba28bd7bb7cd0e29bcd712b7926e57e10a9751c
Ashymad <czilukim@o2.pl>
2018-04-10 21:15:22 +0200

    Plot frequency response

--- code/plotter.py
+++ code/plotter.py
@@ -9,6 +9,8 @@ from scipy.constants import g as gravity
 from mma8451.files import dataread
 import locale
 import sys
+import re
+import os
 import platform
 
 if platform.system() == 'Windows':
@@ -16,33 +18,34 @@ if platform.system() == 'Windows':
 else:
     locale.setlocale(locale.LC_NUMERIC, ('pl_PL', 'UTF-8'))
 
-with h5.File('data.h5','r') as f:
-    data = dataread(f, "2018", '03', '23', '07')
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]  
 
 mrange = 2
 bits = 14
 max_val = 2**(bits-1)
 fs = 800
-
-N = len(data[:,0])
-t = np.array(range(0,N,1))
-t = t/fs
-
-acc = data/max_val*mrange*gravity
-
-frq = acoustics.signal.OctaveBand(fstart=0.5, fstop=350, fraction=3)
-filtbank = acoustics.signal.Filterbank(frq, sample_frequency=fs)
-
+def toint(d): return d/max_val*mrange*gravity
+def rms(d): return np.sqrt(np.mean(np.square(d)))
 
 mpl.use("pgf")
 
 axes = ['x', 'y', 'z']
 
-plot_N = 1000
-frq_rng = [str(s).replace('.', ',') for s in np.round(frq.nominal, 14)]
-bar_width = 0.9
+
 
 def plot1():
+    # Przebieg czasowy pomiaru w D-1
+    with h5.File('data.h5','r') as f:
+        data = toint(dataread(f, "2018", '03', '23', '07'))
+
+    N = len(data[:,0])
+    t = np.array(range(0,N,1))
+    t = t/fs
+
+    plot_N = 1000
+
     for i in range(0,3):
         plt.figure()
         plt.xlabel(r'Czas [\si{\second}]')
@@ -51,6 +54,14 @@ def plot1():
         plt.savefig('../artykul/plots/accel_' + axes[i] + ".pgf")
 
 def plot2():
+    # Wyniki pomiaru w D-1 w postaci tercji
+    with h5.File('data.h5','r') as f:
+        data = toint(dataread(f, "2018", '03', '23', '07'))
+    frq = acoustics.signal.OctaveBand(fstart=0.5, fstop=350, fraction=3)
+    filtbank = acoustics.signal.Filterbank(frq, sample_frequency=fs)
+    frq_rng = [str(s).replace('.', ',') for s in np.round(frq.nominal, 14)]
+    bar_width = 0.9
+
     for i in range(0,3):
         filt = np.sqrt(filtbank.power(acc[:,i]))
         lin = np.arange(len(filt))
@@ -61,15 +72,15 @@ def plot2():
         plt.ylabel(r'Amplituda sygnału [\si{\meter\per\second\squared}]')
         plt.savefig('../artykul/plots/ghost_' + axes[i] + ".pgf")
 
-with h5.File('data2.h5','r') as f:
-    data2 = dataread(f, "2018")
-
-acc2 = data2/max_val*mrange*gravity
-
-plot_S = 1400
-plot_E = 6700
 
 def plot3():
+    # Pomiar obratu w ręce
+    with h5.File('data2.h5','r') as f:
+        data2 = toint(dataread(f, "2018"))
+
+    plot_S = 1400
+    plot_E = 6700
+
     plt.figure()
     plt.xlabel(r'Czas [\si{\second}]')
     plt.ylabel(r'Przyspieszenie drgań [\si{\meter\per\second\squared}]')
@@ -78,9 +89,40 @@ def plot3():
     plt.legend(["oś " + ax for ax in axes])
     plt.savefig('../artykul/plots/accel_2_xyz.pgf')
 
+def plot4():
+    # Charakterystyka częstotliwościowa w osi Y
+    with h5.File('./Rasp/data.frqamp.h5','r') as f:
+        data_mems = dataread(f, "2018", "04", "06", "11", merge=False)
+        data_mems.extend(dataread(f, "2018", "04", "06", "12", [0, 37],
+                                  merge=False))
+        data_mems = [rms(toint(el[:,1]) - np.mean(toint(el[:,1]))) for el in data_mems]
+
+    data_ni = list()
+    path = "./NI/freq_char"
+    files = os.listdir(path)
+    files.sort(key=natural_sort_key)
+    for file in files:
+        data_ni.append(rms(np.loadtxt(path+"/"+file)))
+
+    data_diff = 10*np.log10(np.divide(data_mems,data_ni))
+
+    frq = np.append(np.arange(15, 106, 10),np.arange(150, 400, 50))
+    bar_width = 0.9
+    lin = np.arange(len(data_diff))
+    plt.figure()
+    plt.bar(lin, data_diff, bar_width)
+    plt.xticks(lin, frq, rotation=70)
+    plt.xlabel(r'Częstotliwości badane [\si{\hertz}]')
+    plt.ylabel(r'Róznica [\si{\decibel}]')
+    plt.savefig('../prez/char.pdf')
+
+
+
+
 
 {# This works!
     '1': plot1,
     '2': plot2,
     '3': plot3,
+    '4': plot4,
 }[sys.argv[1]]()
