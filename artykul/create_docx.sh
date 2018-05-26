0a6083f0183b3164c574f8a4e6c3f0bc3dc1a02f
Ashymad <czilukim@o2.pl>
2018-05-27 01:14:22 +0200

    💩x2

--- artykul/create_docx.sh
+++ artykul/create_docx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cp artykul.tex artykul.tmp.tex
-sed '/\\input{.*\.pgf}/s/input/includegraphics/g; s/\.pgf/\.png/g' -i artykul.tmp.tex
+sed '/\\input{.*\.pgf}/s/input/includegraphics/g; s/\.pgf/\.pdf/g' -i artykul.tmp.tex
 
 pandoc --filter pandoc-citeproc\
-    artykul.tmp.tex -f latex -t docx\
+    artykul.tmp.tex -f latex -t markdown\
     --default-image-extension=pdf\
     --bibliography=bibliografia.bib\
     --dpi 11200\
-    -o artykul.docx
+    -o artykul.md
 rm artykul.tmp.tex
